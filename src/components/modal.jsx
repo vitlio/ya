7edb0daaf8c0c3563024023db99ce135eea1e355
vitlio <gorshkov.vitalik@yandex.ru>
2021-06-01 00:04:23 +0300

    fix err

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -6,7 +6,6 @@ export default function Modal(props){
     return(
         <div className="overlay" onClick={(e) => !e.target.closest('.modal-body') && props.closeModal()}>
             <div className="modal-body">
-                {/* <button className="close-modal">x</button> */}
                 <img src={(props.modalItem.cover_i&&(bookCoverLink + 'id/' + props.modalItem.cover_i + '-L.jpg' ))    
         ||(props.modalItem.olid&&(bookCoverLink + 'olid/' + props.modalItem.olid[0] + '-L.jpg' ))
         ||(props.modalItem.isbn&&(bookCoverLink + 'isbn/' + props.modalItem.isbn[0] + '-L.jpg' ))
@@ -15,10 +14,9 @@ export default function Modal(props){
         ||'https://thumbs.dreamstime.com/b/эскиз-книг-рисунки-изображенные-на-рисунках-содержат-кучу-старого-159243668.jpg'} 
         alt={props.modalItem.title}/>
         <span>
-        <p><b>{props.title ? (props.title.length < 50 ? props.title : props.title.substr(0, 50) + '...') : 'Без названия'}</b></p>
-                <p>{props.author ? (props.author.length < 30 ? props.author : props.author.substr(0, 30) + '...') : 'Автор неизвестен'}</p>
+        <p><b>{props.modalItem.title ? props.modalItem.title : 'Без названия'}</b></p>
+                <p>{props.modalItem.author_name ? props.modalItem.author_name : 'Автор неизвестен'}</p>
         </span>
-                {console.log(props.modalItem)}
             </div>
         </div>
     )
